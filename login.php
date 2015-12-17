2defdf328b39fbfd70a5b4d38ae72f51c052b738
Kiara <ramos.torres.belia@gmail.com>
2015-12-17 20:32:18 +0100

    firsts files

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+
+$userName = $_POST["username"];
+$pass = $_POST["password"];
+
+$myfile = fopen("comprueba", "r") or die("Unable to open file!");
+
+$content = fread($myfile, filesize("comprueba"));
+
+$parsedcontents = explode(":", $content);
+
+$fileUserName =  $parsedcontents[0];
+$filePassword = $parsedcontents[1];
+
+if($userName == $fileUserName AND $pass == $filePassword){
+    echo "login successfull";
+} else {
+    echo "incorrect username or password";
+}

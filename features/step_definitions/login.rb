60a5b89236520d8c5bea0d3a405f0d2c92ddc5d8
Adrián Norte <adrian@bashlines.com>
2015-12-25 22:06:13 +0100

    adding more things for testing

---
+++ features/step_definitions/login.rb
@@ -0,0 +1,19 @@
+Given(/^I am on the login page$/) do
+  visit "http://localhost:63342/simple_php_login/login_view.html"
+end
+
+When(/^I input the correct username$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^the correct password$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^click the login button$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see a correct login message$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/support/env.rb
@@ -0,0 +1,11 @@
+require 'rspec/expectations'
+require 'capybara'
+require 'capybara/mechanize'
+require 'capybara/cucumber'
+require 'test/unit/assertions'
+require 'mechanize'
+
+World(Test::Unit::Assertions)
+Capybara.default_driver = :mechanize
+Capybara.app_host = "http://localhost:63342"
+World(Capybara)
